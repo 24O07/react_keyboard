6fd0ecb617f6ca764e8201d3937ecc74a4b04912
Olga Betsenko <betsenkoo@gmail.com>
2026-04-06 14:41:05 +0300

    add task solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,35 @@
 import React from 'react';
 
-
 type State = {
-  pressedKey : string | null;
-}
+  pressedKey: string | null;
+};
 
-export class App extends React.Component<{},State> {
-  state:State ={
-    pressedKey:null,
+export class App extends React.Component<{}, State> {
+  state: State = {
+    pressedKey: null,
   };
 
-  handleKeyUp = (event:KeyboardEvent)=>{
-    this.setState({pressedKey : event.key});
+  handleKeyUp = (event: KeyboardEvent) => {
+    this.setState({ pressedKey: event.key });
   };
 
-  componentDidMount(){
-    document.addEventListener('keyup',this.handleKeyUp);
+  componentDidMount() {
+    document.addEventListener('keyup', this.handleKeyUp);
   }
 
-  componentWillUnmount(){
-    document.removeEventListener('keyup',this.handleKeyUp);
+  componentWillUnmount() {
+    document.removeEventListener('keyup', this.handleKeyUp);
   }
 
-  render(){
-    const {pressedKey} = this.state;
+  render() {
+    const { pressedKey } = this.state;
 
-    return(
+    return (
       <div className="App">
-        <p className="App_message">
+        <p className="App__message">
           {pressedKey
-          ? `The last pressed key is [${pressedKey}]`
-          : 'Nothing was pressed yet'
-          }
+            ? `The last pressed key is [${pressedKey}]`
+            : 'Nothing was pressed yet'}
         </p>
       </div>
     );
